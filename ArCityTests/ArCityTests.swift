26f9b5009e88f03cc01024ab3f95d0b118873586
Vladislav Ainshtein <vladislav.ainshtein@gmail.com>
2025-03-14 14:30:59 +0700

    Initial Commit

---
+++ ArCityTests/ArCityTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArCityTests.swift
+//  ArCityTests
+//
+//  Created by Vladislav Ainshtein on 14.03.25.
+//
+
+import Testing
+@testable import ArCity
+
+struct ArCityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
